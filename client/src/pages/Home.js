6c4e3796958b37d6ea00889bfa2c6cd63aedf145
MarynaPR <pryadkamaryna@gmail.com>
2021-02-05 13:00:27 -0600

    friend and thought components added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import ThoughtList from '../components/ThoughtList';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHTS } from '../utils/queries';
-import ThoughtList from '../components/ThoughtList';
 
 
 
 const Home = () => {
   // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
-
 
   const thoughts = data?.thoughts || [];
   console.log(thoughts);
